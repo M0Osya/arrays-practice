d130253b6718c798c7f8a02f3f9c5446085ac479
Колобаев <ggpoiggpoi5@gmail.com>
2026-09-26 00:54:20 +0300

    Task1: статистика случайного массива

--- Task1.Statistics/Program.cs
+++ Task1.Statistics/Program.cs
@@ -1,52 +1,43 @@
-using System;
-using System.Globalization;
+﻿using System;
 
-namespace Task1.Statistics
+class Program
 {
-    class Program 
+    static void Main()
     {
-        static void Main(string[] args)
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = random.Next(1, 101);
+
+        Console.WriteLine("Массив: " + string.Join(", ", numbers));
+
+        int sum = 0;
+        long product = 1;
+        int evenCount = 0;
+
+        foreach (int number in numbers)
         {
-            Random random = new Random(100);
-            int[] numbers = new int[10];
+            sum += number;
+            product *= number;
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = random.Next(1, 101);
-            }
-            Console.WriteLine($"Массив: {string.Join(", ", numbers)}");
-
-            int sum = 0;
-            double product = 1;
-            int count = 0;
-
-            foreach(int num in numbers)
-            {
-                sum += num;
-                product *= num;
-                if (num % 2 == 0)
-                {
-                    count++;
-                }
-            }
-            
-            double average = (double)sum / numbers.Length;
-
-            int greaterCount = 0;
-            foreach(int num in numbers)
-            {
-                if (num > average)
-                {
-                    greaterCount++;
-                }
-            }
-
-            Console.WriteLine($"Сумма {sum}");
-            Console.WriteLine($"Произведение {product:E2}");
-            Console.WriteLine($"Четных чисел {count}");
-            Console.WriteLine($"Больше среднего ({Math.Round(average, 1)}): {greaterCount}");
-
+            if (number % 2 == 0)
+                evenCount++;
         }
-    }
 
+        double average = (double)sum / numbers.Length;
+
+        int aboveAverage = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > average)
+                aboveAverage++;
+        }
+
+        Console.WriteLine("Сумма: " + sum);
+        Console.WriteLine("Произведение: " + product);
+        Console.WriteLine("Чётных чисел: " + evenCount);
+        Console.WriteLine($"Больше среднего ({average:F1}): {aboveAverage}");
+    }
 }
